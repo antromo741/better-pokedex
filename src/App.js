5fd77fd4cceb2813f2b19be5e8c04cbf65da5202
Anthony_Romagnolo <anthonyroma741@gmail.com>
2021-04-06 14:58:31 -0400

    added some css, cant get pokemon background to render properly, sometimes it comes, sometimes it does not

--- src/App.js
+++ src/App.js
@@ -10,16 +10,16 @@ const App = () => {
   const getPokemon = async () => {
     const toArray = [];
     try {
-      const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`
-      const res = await axios.get(url)
-      toArray.push(res.data)
+      const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
+      const res = await axios.get(url);
+      toArray.push(res.data);
       setPokemonType(res.data.types[0].type.name);
-      setPokemonData(toArray)
-      console.log(res)
+      setPokemonData(toArray);
     } catch (e) {
-      console.log(e)
+      console.log(e);
     }
-  }
+  };
+  console.log(pokemonData);
 
 
   const handleChange = (e) => {
@@ -42,40 +42,38 @@ const App = () => {
         {pokemonData.map((data) => {
           //Def need to come back and refactor with tailwind
           //Perhaps display some base stats on the card component
-          return(
-          <div className="container">
-            
-            <img src={data.sprites["front_default"]}/>
-
-            <div className="divTable">
-              <div className="divTableBody">
-                <div className="divTableRow">
-                  <div className="divTableCell">Type</div>
-                  <div className="divTableCell">{pokemonType}</div>
-                </div>
-
-                <div className="divTableRow">
-                  <div className="divTableCell">Height</div>
-                  <div className="divTableCell">
-                    {" "}
-                    {Math.round(data.height * 3.9)}
-                    </div>
-                </div>
-
-                <div className="divTableRow">
-                  <div className="divTableCell">Weight</div>
-                  <div className="divTableCell">
-                    {" "}
-                    {Math.round(data.weight / 4.3 )} lbs
+          return (
+            <div className="container">
+              <img 
+              src={data.sprites["front_default"]}
+              alt={" "} 
+              />
+              <div className="divTable">
+                <div className="divTableBody">
+                  <div className="divTableRow">
+                    <div className="divTableCell">Type</div>
+                    <div className="divTableCell">{pokemonType}</div>
+                  </div>
+                  <div className="divTableRow">
+                    <div className="divTableCell">Height</div>
+                    <div className="divTableCell">
+                      {" "}
+                      {Math.round(data.height * 3.9)}"
+                  </div>
+                  </div>
+                  <div className="divTableRow">
+                    <div className="divTableCell">Weight</div>
+                    <div className="divTableCell">
+                      {" "}
+                      {Math.round(data.weight / 4.3)} lbs
+                  </div>
+                  </div>
+                  <div className="divTableRow">
+                    <div className="divTableCell">Number of Battles</div>
+                    <div className="divTableCell">{data.game_indices.length}</div>
                   </div>
                 </div>
-
-                <div className="divTableRow">
-                  <div className="divTableCell">Number of Battles</div>
-                  <div className="divTableCell">{data.game_indices.length}</div>
-                </div>
               </div>
-            </div>
             </div>
           );
         })}
